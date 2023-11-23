0661049053c645272180665ab2aa5be612fb52d3
microcrafted <25.axel.rafael@gmail.com>
2023-11-22 21:53:02 -0600

    primera y menu completo

--- proyecto candelario.cpp
+++ proyecto candelario.cpp
@@ -6,7 +6,10 @@ int main() {
     int vida = 100; // este será el recolectable
     int opcion;     // este se usará para interactuar con el menú principal
     int historia;   // esta se usará para poder moverte en la historia
-
+	int historia2;   // esto sera el segundo punto interactivo en la historia
+	int historia3;   // tercer punto interactivo en la historia
+	int historia4;   // cuarto punto interactivo en la historia
+	int historia5;  //quinto y ultimo punto de interaccion dentro de esta historia
     do {
         cout << "Bienvenido a la historia interactiva. En esta, se te contará una historia con diferentes caminos a elegir." << endl;
         cout << "Espero que sea de tu agrado y te diviertas!" << endl;
@@ -14,14 +17,13 @@ int main() {
         cout << "1. Iniciar partida" << endl;
         cout << "2. Informacion de contacto" << endl;
         cout << "3. Clase y profesor" << endl;
-        cout << "4. Marco" << endl;
+        cout << "4. Calculadora" << endl;
         cout << "5. Salir del juego" << endl;
         cout << "Por favor, ingresa tu eleccion: ";
         cin >> opcion;
 
-        switch (opcion) {   // aquí pondremos el número de la opción a elegir :b
+        switch (opcion) {   // aquí pondremos el número de la opción a elegir :O
             case 1:
-                do {
                     cout << "Tu historia empieza aquí. Eres un aventurero buscando el tan aclamado elixir de la vida en el antiguo reino de Nezahualcóyotl, y estás en búsqueda del poderoso elixir de la vida, ya que con este se logrará tener la inmortalidad." << endl;
                     cout << "Actualmente, tu vida es: " << vida << "% y a continuación diremos las pruebas a seguir para conseguir el elixir" << endl;
                     cout << "Según la leyenda hay 3 posibles formas de poder llegar a este increíble tesoro. La primera forma es cruzando el terrorífico bosque oscuro donde se encuentran arañas gigantes. La segunda forma de ir es por el sendero iluminado en donde se encuentra un humo tóxico el cual te quitará vida mientras lo pasas. La tercera y última forma es ir y explorar la cueva misteriosa, la cual no se encuentra con ningún registro de que alguien haya podido pasar por ella." << endl;
@@ -30,22 +32,11 @@ int main() {
                     cout << "2. Sendero iluminado" << endl;
                     cout << "3. Cueva misteriosa" << endl;
                     cin >> historia;
-
-                    switch (historia) {
-                        case 1:
-                            cout << "Entraste al bosque oscuro y te enfrentaste a una araña. Perdiste 15% de vida. Ahora tienes: " << vida - 15 << "% de vida." << endl;
-                            break;
-                        case 2:
-                            cout << "Tomaste el sendero iluminado y enfrentaste un humo tóxico. Perdiste 10% de vida. Ahora tienes: " << vida - 10 << "% de vida." << endl;
-                            break;
-                        case 3:
-                            cout << "Exploraste la cueva misteriosa. Encontraste un tesoro y ganaste 20% de vida. Ahora tienes: " << vida + 20 << "% de vida." << endl;
-                            break;
-                        default:
-                            cout << "Elige una opción válida." << endl;
-                            break;
-                    }
-                } while (historia != 3);
+                    	if(historia == 1) {
+                    		
+						}
+                    
+                    
 
                 cout << "Volviendo al menú" << endl;
                 break;
@@ -65,9 +56,42 @@ int main() {
                 break;
 
             case 4:       
-                cout << "Polo" << endl; // Puedes agregar contenido relacionado con Marco aquí.
-                cout << "Volviendo al menú principal." << endl;
-                break;
+					char operador;
+    				double numero1, numero2;
+
+					cout << "Ingresa un operador (+, -, *, /): ";
+    				cin >> operador;
+
+    				cout << "Ingresa dos números: ";
+    				cin >> numero1 >> numero2;
+
+				    double resultado;
+
+				    switch (operador) {
+        				case '+':
+            				resultado = numero1 + numero2;
+            			break;
+        				case '-':
+            				resultado = numero1 - numero2;
+            			break;
+        				case '*':
+            				resultado = numero1 * numero2;
+            			break;
+        				case '/':
+           				 if (numero2 != 0) {
+            				    resultado = numero1 / numero2;
+           					} else {
+                				cout << "Error: No se puede dividir por cero." << endl;
+                			return 1;  // Salir del programa con un código de error
+            				}
+            			break;
+        					default:
+        					    cout << "Operador no válido. Por favor, ingresa un operador válido (+, -, *, /)." << endl;
+       					    return 1;  // Salir del programa con un código de error
+   					 }
+
+   					 cout << "El resultado es: " << resultado << endl;
+						  break;
 
             case 5:       // salir del juego
                 cout << "¡Gracias por jugar!" << endl;
